ba372399d2d7b0599fbd83a3ca84539dc66bfb3a
Leonardo <leonardo@leonardo-VIT-P3400.(none)>
2015-07-09 21:17:46 -0430

    Secon modification of the file new course

--- server.R
+++ server.R
@@ -11,12 +11,12 @@ shinyServer(
          #datos$prof=-datos$prof
 		 #datos$time <- as.Date(paste(datos$YYYY,datos$mm,datos$dd,sep="-"))
 		  load("datos.RData",.GlobalEnv)
-		  datos1 <- subset(datos, YYYY>= 2000 & YYYY<=input$mu & lat>=input$latmin & lat <=input$latmax &lon>=input$lonmin & lon<=input$lonmax &datos$prof<300)
-		  xrang <- c(seq(floor(min(datos1$mag)),ceiling(max(datos1$mag)),by=1))
+		  datos1 <- subset(datos, Year>= 2000 & Year<=input$mu & lat>=input$latmin & lat <=input$latmax &lon>=input$lonmin & lon<=input$lonmax &datos$prof<300)
+		  xrang <- c(seq(floor(min(datos1$Magnitude)),ceiling(max(datos1$Magnitude)),by=1))
 
-		  gg <- ggplot(datos1, aes_string(x=input$x)) + geom_histogram(binwidth=1,colour="black",fill="blue") +
-	  	  ylab("No sismos") + theme(axis.text.x = element_text(angle = 0, hjust = 1,size=14,colour="black"),
-        	axis.title=element_text(size=20,face="bold"), axis.text.y = element_text(hjust = 1,size=14,colour="black")) 
+		  gg <- ggplot(datos1, aes_string(x=input$x)) + geom_histogram(binwidth=1,colour="black",fill="blue") + 
+	  	  ylab("Counts") +ggtitle("Histograms") + theme(axis.text.x = element_text(angle = 0, hjust = 1,size=14,colour="black"),
+        	axis.title=element_text(size=16,face="bold"), axis.text.y = element_text(hjust = 1,size=14,colour="black")) 
 			#+ scale_x_continuous(limit=(0,1))
 	  
 		  print(gg)
--- ui.R
+++ ui.R
@@ -1,17 +1,24 @@
 #datos <- read.table("FUNVISIS_SD.txt",header=T)
 shinyUI(pageWithSidebar(
-	headerPanel("Histograms Magnitud / Year"),
+	headerPanel("Histograms: Magnitud - Year"),
 	sidebarPanel(
-		sliderInput('mu','Year',value=2010,min=2000,max=2010,step=1),
-		numericInput('latmin','Lat min', 5.0),
-		numericInput('latmax','Lat max', 14.0),
-		numericInput('lonmin','Lon min', -74.0),
-		numericInput('lonmax','Lon max', -58.0),
-        selectInput('x','X',c("Year"="YYYY","Magnitud"="mag"))
+		sliderInput('mu','Plot event from Year',value=2010,min=2000,max=2010,step=1),
+		h4('Region'),
+		numericInput('latmin','Latitude min', 5.0),
+		numericInput('latmax','Latitude max', 14.0),
+		numericInput('lonmin','Longitude min', -74.0),
+		numericInput('lonmax','Longitude max', -58.0),
+		h4('Type of histogram'),
+        selectInput('x','Histogram',c("Year"="Year","Magnitud"="Magnitude"))
 		),
 	mainPanel(
+		tabsetPanel(
+			tabPanel("Documentation",includeHTML("Documento.html")),
+			tabPanel("Histograms",
 		plotOutput('newHist'),
-		imageOutput("myImage")
-		)
+		imageOutput("myImage"))
+		
+		
+		))
 	  )
 	)
